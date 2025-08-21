c3f4b5d1a6af1d496b91aef174c13ce25bd234d0
Dmitriy Voronin <devoronin@gmail.com>
2025-08-21 12:24:18 +0300

    Add Firebase

---
+++ PhotoEditorWithAuthApp/PhotoEditorWithAuthApp.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoEditorWithAuthApp.swift
+//  PhotoEditorWithAuthApp
+//
+//  Created by Dmitrii Voronin on 21.08.2025.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+@main
+struct PhotoEditorWithAuthApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
---
+++ PhotoEditorWithAuthApp/ViewModels/AuthViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewModel.swift
+//  PhotoEditorWithAuthApp
+//
+//  Created by Dmitrii Voronin on 21.08.2025.
+//
+
+import Combine
+
+class AuthViewModel: ObservableObject {
+    @Published var isAithentificated: Bool = false
+}
